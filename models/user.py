ece78bf34e1df59d6d00317d23f15491f850811a
Gideon Mba <abaaneg@gmail.com>
2023-09-20 16:57:46 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+from os import getenv
 from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from models.base_model import BaseModel, Base
+
 
 class User(BaseModel, Base):
-    """Represents a :wq
-    user for a MySQL database."""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    if getenv('HBNB_TYPE_STORAGE', "fs") != "db":
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+    else:
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
