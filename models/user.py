03aaa1487bde658bcacdba2ad9f480a3f07724e5
Gideon Mba <abaaneg@gmail.com>
2023-09-20 13:43:51 -0700

    user place relationship

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -16,7 +17,6 @@ class User(BaseModel):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
     else:
     """This class defines a user by various attributes"""
         email = ''
