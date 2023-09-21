a5db030ebe431517cf95f7fe350d3cbceb48f6dc
Gideon Mba <abaaneg@gmail.com>
2023-09-20 17:03:06 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-from os import getenv
-from models import *
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    if getenv('HBNB_TYPE_STORAGE', "fs") != "db":
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-    else:
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    __table_args__ = ({'mysql_default_charset': 'latin1'})
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
